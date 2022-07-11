cde4254d61aa0ac0aed9492d98edcaf724cdfb4e
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2022-07-10 22:58:24 -0400

    Setup unittest package

---
+++ Tests/test_card.py
@@ -0,0 +1,14 @@
+import unittest
+from Cards import Card, Deck, Shoe, Value, Suite
+
+
+class TestCards(unittest.TestCase):
+    def setUp(self) -> None:
+        card = Card(Suite.CLUB, Value.ACE)
+
+    def test_card(self) -> None:
+        self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
