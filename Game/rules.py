bbb106d988d0027db1211372b6719144bf13b2ef
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2022-07-11 17:09:53 -0400

    Reduce complexity for ace rules.

--- Game/rules.py
+++ Game/rules.py
@@ -1,41 +1,16 @@
 from Cards import Value, Card
 
-ACE_DIFFERENCE = 11 - 1
-
 
 class Rules:
     def __init__(self, player_hand: list[Card]):
-        self.hand = player_hand
 
-    def is_blackjack(self) -> bool:
-        if self.hand_value() == 21 and len(self.hand) == 2:
-            return True
-
-    def is_bust(self) -> bool:
-        """Determine if the player exceeds a score of 21."""
-        if self.hand_value() > 21:
-            return True
-
-    def hand_value(self) -> int:
-        """evaluates player's hand"""
-
-        card_values = [
-            self.blackjack_value(card.value)
-            for card in self.hand
+        self.hand_value_list = [
+            self.__blackjack_value(card.value)
+            for card in player_hand
         ]
 
-        value_sum = sum(card_values)
-
-        # change aces if they bust
-        count_aces = card_values.count(11)  # count aces in list
-
-        if count_aces > 0 and value_sum > 21:
-            return value_sum - (count_aces * ACE_DIFFERENCE)
-
-        return value_sum
-
     @staticmethod
-    def blackjack_value(card_value: Value) -> int:
+    def __blackjack_value(card_value: Value) -> int:
         """Returns the blackjack value of a card."""
         val_table = {
             Value.TWO: 2, Value.THREE: 3, Value.FOUR: 4, Value.FIVE: 5,
@@ -45,3 +20,23 @@ class Rules:
         }
 
         return val_table[card_value]
+
+    def hand_value(self) -> int:
+        """evaluates player's hand"""
+
+        # If player busts, replace aces with value = 1
+        for idx, val in enumerate(self.hand_value_list):
+            bust_with_aces = (val == 11) and sum(self.hand_value_list) > 21
+            if bust_with_aces:
+                self.hand_value_list[idx] = 1
+
+        return sum(self.hand_value_list)
+
+    def is_blackjack(self) -> bool:
+        if self.hand_value() == 21 and len(self.hand_value_list) == 2:
+            return True
+
+    def is_bust(self) -> bool:
+        """Determine if the player exceeds a score of 21."""
+        if self.hand_value() > 21:
+            return True
