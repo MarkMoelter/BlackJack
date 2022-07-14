a6c15ec72dc23860d08b55c6b48755a3d64a2c99
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2022-07-13 21:28:47 -0400

    Delete actions.py and table.py

--- Game/__init__.py
+++ Game/__init__.py
@@ -1,6 +1,4 @@
-from .action import Action
 from .dealer import Dealer
 from .game import Game
 from .player import Player
 from .rules import Rules
-from .table import Table
--- Game/actions.py
+++
@@ -1,5 +0,0 @@
-from Cards import Card, Shoe
-
-
-def hit(shoe: Shoe) -> Card:
-    return shoe.deal()
